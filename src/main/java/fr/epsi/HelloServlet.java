710bbd88172d6c0836644478becf2d40a5042422
SAM <samuel.dulhoste@gmail.com>
2017-03-24 15:13:55 +0100

    ajout des autres fichiers

---
+++ src/main/java/fr/epsi/HelloServlet.java
@@ -0,0 +1,30 @@
+package fr.epsi;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+
+  /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+@Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp) 
+                 throws ServletException, IOException {
+    req.setCharacterEncoding("utf-8");
+    String name = req.getParameter("name");
+
+    resp.setContentType("text/plain");
+    resp.setCharacterEncoding("utf-8");
+    resp.getWriter().write("Hello " + name + "!");
+  }
+
+}
